e985cb11fd741e1303cea9f2a424bce20f8cbfb5
victoriarios-blip <victoria.rios@ing.unlpam.edu.ar>
2026-05-18 17:20:02 -0300

    sist de juegos final ;)

--- src/main/java/pipoo/core/SistemaDeJuegos.java
+++ src/main/java/pipoo/core/SistemaDeJuegos.java
@@ -4,37 +4,47 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-import pipoo.core.Juego;
+import pipoo.loderunner.LodeRunner;
 import pipoo.pong.Pong;
-// import spaceinvaders.SpaceInvaders;
-// import loderunner.LodeRunner;
+import pipoo.spaceinvaders.SpaceInvaders;
 
 public class SistemaDeJuegos extends JPanel implements ActionListener {
     Juego juegoActual;
     Thread hiloJuego;
 
     public SistemaDeJuegos() {
-        setLayout(new GridLayout(1, 3, 10, 10)); // Botones en fila
+        setLayout(new GridLayout(1, 3, 10, 10));
 
-        JButton btnPong = new JButton("Jugar Pong");
+        JButton btnPong = new JButton("Pong");
         btnPong.addActionListener(this);
         add(btnPong);
 
-        // Agregar botones para Lode Runner y Space Invaders aquí...
+        JButton btnLodeRunner = new JButton("Lode Runner");
+        btnLodeRunner.addActionListener(this);
+        add(btnLodeRunner);
+
+        JButton btnSpaceInvaders = new JButton("Space Invaders");
+        btnSpaceInvaders.addActionListener(this);
+        add(btnSpaceInvaders);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("Jugar Pong")) {
-            //juegoActual = new Pong();
-            // Inicia el juego en un hilo separado como pide el recurso de la cátedra
+        if (e.getActionCommand().equals("Pong")) {
+            juegoActual = new Pong();
+        }
+        if (e.getActionCommand().equals("Lode Runner")) {
+            juegoActual = new LodeRunner();
+        }
+        if (e.getActionCommand().equals("Space Invaders")) {
+            juegoActual = new SpaceInvaders();
+        }
             hiloJuego = new Thread(() -> juegoActual.run(1.0 / 60.0));
             hiloJuego.start();
-        }
     }
 
     public static void main(String[] args) {
-        JFrame ventanaMenu = new JFrame("Retro Reobot 2026 - Menú Principal");
+        JFrame ventanaMenu = new JFrame("PIPOO Menú Principal");
         ventanaMenu.add(new SistemaDeJuegos());
         ventanaMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventanaMenu.pack();
