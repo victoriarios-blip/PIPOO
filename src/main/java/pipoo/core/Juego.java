0e262546eb314f2a538909fe467d1e6a2fff41d7
victoriarios-blip <victoria.rios@ing.unlpam.edu.ar>
2026-05-18 16:53:26 -0300

    juego final ;)

--- src/main/java/pipoo/core/Juego.java
+++ src/main/java/pipoo/core/Juego.java
@@ -11,27 +11,18 @@ public abstract class Juego extends JGame {
         super(titulo, ancho, alto);
 }
 
-    // Estos métodos de su UML ahora los forzamos a implementarse en cada juego particular
     protected abstract void actualizarPuntaje();
     protected abstract void detectarColisiones();
 
-    // Los métodos abstractos obligatorios heredados de JGame [8-10] que cada juego definirá:
     @Override
-    public void gameStartup() {
-
-    }
+    public void gameStartup() {}
 
     @Override
-    public void gameUpdate(double v) {
-
-    }
+    public void gameUpdate(double v) {}
 
     @Override
-    public void gameDraw(Graphics2D graphics2D) {
-
-    }
+    public void gameDraw(Graphics2D graphics2D) {}
 
     @Override
-    public void gameShutdown(){
-    }
+    public void gameShutdown(){}
 }
