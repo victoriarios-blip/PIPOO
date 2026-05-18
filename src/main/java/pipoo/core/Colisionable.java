810c7b1c70a58984603753cbf5ec3dd231f18a25
victoriarios-blip <victoria.rios@ing.unlpam.edu.ar>
2026-05-18 16:30:00 -0300

    pequeñas correcciones

--- src/main/java/pipoo/core/Colisionable.java
+++ src/main/java/pipoo/core/Colisionable.java
@@ -4,6 +4,8 @@ import java.awt.Shape;
 public interface Colisionable {
     public Shape getLimites();
 
+    void reaccionarAColision();
+
     //boolean colisinaCon(Colisionable otro);
     //void reaccionarAColision(Colisionable otro);
 }
