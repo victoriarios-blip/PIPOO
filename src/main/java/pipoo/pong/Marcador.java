24c3263f5d1c7ae87e03b3b1ac87b1765823eb0a
Alejo Hidalgo <alejo.hidalgo@ing.unlpam.edu.ar>
2026-05-20 15:52:56 -0300

    commit varios

--- src/main/java/pipoo/pong/Marcador.java
+++ src/main/java/pipoo/pong/Marcador.java
@@ -14,5 +14,15 @@ public class Marcador extends ElementoGrafico {
     public void dibujar(Graphics2D g) { } // muestra el score en pantalla
     public void sumarPuntoJ1(){ }
     public void sumarPuntoJ2(){ } // incrementar puntaje
+
+    @Override
+    public boolean colisionaCon(ElementoGrafico otro) {
+        return false;
+    }
+
+    @Override
+    public void reaccionarAColision(ElementoGrafico otro) {
+
+    }
     //public boolean hayGanador() { } // gana el que alcanza a 11 o 15 puntos
 }
