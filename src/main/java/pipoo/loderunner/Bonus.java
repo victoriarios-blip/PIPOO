ede85afe57c708218835de7dc11fc9a38a980947
martinaolguin-hub <martina.olguin@ing.unlpam.edu.ar>
2026-05-26 16:36:17 -0300

    LodeRunner Bonus

--- src/main/java/pipoo/loderunner/Bonus.java
+++ src/main/java/pipoo/loderunner/Bonus.java
@@ -3,7 +3,17 @@ package pipoo.loderunner;
 import pipoo.core.ElementoGrafico;
 
 public class Bonus extends ElementoGrafico {
-    public Bonus(double x, double y, int ancho, int alto) {
-        super(x, y, ancho, alto);
+    public Bonus(double x, double y,double width, double height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public boolean colisionaCon(ElementoGrafico otro) {
+        return false;
+    }
+
+    @Override
+    public void reaccionarAColision(ElementoGrafico otro) {
+
     }
 }
