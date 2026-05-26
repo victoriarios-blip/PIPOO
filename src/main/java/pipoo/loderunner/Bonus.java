b6cecd2e7b34886178a1126d0482bf769454377b
victoriarios-blip <victoria.rios@ing.unlpam.edu.ar>
2026-05-26 16:40:45 -0300

    bonus

--- src/main/java/pipoo/loderunner/Bonus.java
+++ src/main/java/pipoo/loderunner/Bonus.java
@@ -6,4 +6,14 @@ public class Bonus extends ElementoGrafico {
     public Bonus(double x, double y, int ancho, int alto) {
         super(x, y, ancho, alto);
     }
+
+    @Override
+    public boolean colisionaCon(ElementoGrafico otro) {
+        return false;
+    }
+
+    @Override
+    public void reaccionarAColision(ElementoGrafico otro) {
+
+    }
 }
